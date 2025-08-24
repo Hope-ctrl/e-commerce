ab550d2de9dc85be4589e8bb6c66f3ea54d5d377
Hope-ctrl <adebara432@gmail.com>
2025-08-23 22:49:59 -0700

    context creation

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -1,6 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const CartContext = createContext()
+const CartContext = createContext();
+
+export const CartProvider = ({children})=>{
+    const [cart, setCart] = useState([]);
+
+    return(
+        <CartContext.Provider>
+            {children}
+        </CartContext.Provider>
+    )
+}
 
 export default CartContext;
-
